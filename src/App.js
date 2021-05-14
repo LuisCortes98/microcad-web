267f68b7f1c3faff92723c3f39d2a809dd908e1a
lcortes@neu.com.co+1-at-782580752539 <luism.cortesj@gmail.com>
2021-05-13 22:40:59 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Home from './components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './icons/css/all.min.css';
+import './style/style.scss';
+
+export default class App extends Component {
+    static displayName = App.name;
+
+    render() {
+        return (
+            <section className="row-start align-items-start vh-100 w-100 bg-white flex-grow-1 h-100">
+                <Home/>
+            </section>
+        );
+    }
 }
-
-export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="w-100 bg-black">
+            <div className="txt-semi-bold txt-violet txt-30">¿Sabes que software quieres?</div>
+        </div>
+    )
+}
+
+export default Home;
